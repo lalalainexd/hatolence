cc6d63282f83bd4c182fc8fae9e1645cda640d44
lalalainexd <lalalainexd@gmail.com>
2013-06-01 18:09:11 -0600

    added bias class

--- lib/hatolence/bias.rb
+++ lib/hatolence/bias.rb
@@ -5,5 +5,16 @@ module Hatolence
       result = RestClient.get "http://localhost:8080/api/biases.json"
       JSON.parse(result)
     end
+
+    def self.filter_by *category
+      result = RestClient.get "http://localhost:8080/api/biases.json?#{categories(category)}"
+      JSON.parse(result)
+    end
+
+    def self.categories category
+
+      blah = category.collect{|c|"category[]=#{c}"}.join("&")
+
+    end
   end
 end
--- spec/hatolence/bias_spec.rb
+++ spec/hatolence/bias_spec.rb
@@ -3,12 +3,25 @@ require 'spec_helper'
 module Hatolence
   describe Bias do
 
-    xit "returns a list of all biases" do
-      expect(Bias.all.count).to eq 2
+    it "returns a list of all biases" do
+      VCR.use_cassette('bias_count') do
+        expect(Bias.all.count).to eq 4
+      end
     end
 
     describe "filter_by" do
-      xit "returns all biases of a specific bias type" do
+      it "returns all biases of a specific bias category" do
+        VCR.use_cassette('land_bias_count') do
+          result = Bias.filter_by("land")
+          expect(result.count).to eq 2
+        end
+      end
+
+      it "returns all biases of a multiple bias categories" do
+        VCR.use_cassette('land_and_water_bias_count') do
+          result = Bias.filter_by("land", "water")
+          expect(result.count).to eq 4
+        end
 
       end
     end
